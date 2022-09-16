515fd041416cf1f3c7ef87710e4dd055afe6e9c4
isisgodoy <113685928+isisgodoy@users.noreply.github.com>
2022-09-16 07:56:16 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("start")
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+})
 basic.forever(function () {
 	
 })
