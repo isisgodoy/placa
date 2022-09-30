91e86f53ef53ef0bacdc2a57a93e66eec83fadb9
isisgodoy <113685928+isisgodoy@users.noreply.github.com>
2022-09-30 08:00:06 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,8 +1,49 @@
+let tocar = false
+let caixa_P2 = 0
+let caixa = 0
 input.onPinPressed(TouchPin.P0, function () {
+    tocar = false
     basic.showString("start")
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
+    basic.clearScreen()
+    basic.pause(randint(2000, 5000))
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (tocar == false) {
+        tocar = true
+        caixa_P2 += 1
+        basic.showLeds(`
+            . . # # #
+            . . # # #
+            . . # # #
+            . . # # #
+            . . # # #
+            `)
+        basic.pause(100)
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (tocar == false) {
+        tocar = true
+        caixa += 1
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            `)
+        basic.pause(100)
+    }
 })
 basic.forever(function () {
 	
